cd3efdb4df9965b9c422c0b8966bef9a1df1c85f
tatus <tatusolavi@gmail.com>
2026-03-28 00:44:50 +0200

    Made the architecture bit cleaner

---
+++ Toolkit_API/Application/App_Services/User/Login.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+using Toolkit_API.Application.Interfaces;
+using Toolkit_API.Domain.Entities.Auth;
+using Toolkit_API.DTOs.UserDTOs;
+using Toolkit_API.Middleware.Exceptions;
+
+namespace Toolkit_API.Application.App_Services.User
+{
+    public class Login
+    {
+        private readonly IUserRepo _userRepo;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IGenerateToken _tokenGenerator;
+        public Login(IUserRepo userRepo, IPasswordHasher passwordHasher,IGenerateToken generateToken)
+        {
+            _passwordHasher = passwordHasher;
+            _userRepo = userRepo;
+            _tokenGenerator = generateToken;
+        }
+        public async Task<string> LoginMethod(LoginDTO loginDTO)
+        {
+
+            byte[] saltBytes;
+
+            var passwordHash = _passwordHasher.HashPassword(loginDTO.password, out saltBytes); 
+            
+            var user = await _userRepo.Login(loginDTO.username,passwordHash,saltBytes);
+
+            if (user == null) 
+                throw new UnauthorizedAccessException();
+
+            if (!_passwordHasher.VerifyPassword(loginDTO.password, passwordHash, saltBytes)) 
+                throw new UnauthorizedAccessException();
+
+            return _tokenGenerator.GenerateToken(user);
+        }
+
+    }
+}
--- Toolkit_API/Controllers/AuthController.cs
+++ Toolkit_API/Controllers/AuthController.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Toolkit_API.Application.App_Services.User;
+using Toolkit_API.DTOs.UserDTOs;
 
 namespace Toolkit_API.Controllers
 {
-    public class AuthController : Controller
+    [Route("/Login")]
+    public class AuthController : ControllerBase
     {
-        
+        private readonly Login _login;
+        public AuthController(Login login) 
+        { 
+            _login = login;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login([FromHeader]LoginDTO loginDTO)
+        {
+            var result = await _login.LoginMethod(loginDTO);
+            return Ok(result);
+        }
     }
 }
